566d2da0bfbe4432fc806b1ba1fa40785841dc14
Robert Schönthal <caziel@gmx.net>
2015-01-14 16:32:10 +0100

    added DB authenticatation

--- src/MongoClient.php
+++ src/MongoClient.php
@@ -594,6 +594,7 @@ class MongoClient
     {
         if (!isset($this->databases[$name])) {
             $this->databases[$name] = new MongoDB($this, $name);
+            $this->databases[$name]->authenticate($this->_getAuthenticationUsername(), $this->_getAuthenticationPassword());
         }
 
         return $this->databases[$name];
